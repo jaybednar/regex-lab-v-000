0185864749acb733179feec46570fab42facd9cb
Jay Bednar <ironviralmedia@gmail.com>
2018-02-10 00:56:30 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
 def starts_with_a_vowel?(word)
-  !!word.match(/\b^[aeiou]/) 
+  !!word.match(/^[aeiouAEIOU]/) 
   # binding.pry
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\w\b+un+\b+ing/)
+  text.scan(/\w\b+un+ing/)
 end
 
 def words_five_letters_long(text)
