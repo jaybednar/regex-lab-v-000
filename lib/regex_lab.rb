a43014567ae42e21540784fc98bfdd15400f47d1
Jay Bednar <ironviralmedia@gmail.com>
2018-02-10 00:08:21 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,7 @@
 require 'pry'
 
 def starts_with_a_vowel?(word)
-  if word.match(/\A+[aeiou]/) == nil
-    # binding.pry
-    return false
-  else
-    return true
-  end
+  word.match(/\A+[aeiou]/) != nil
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
