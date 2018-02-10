9f2e2756e1e0201a20d70033abdc829f486745d0
Jay Bednar <ironviralmedia@gmail.com>
2018-02-10 01:11:31 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\b[A-Z]\w+\W\D\b/)
+  if !!text.match(/\b[A-Z]\w+\W\D\b/)
+    return true
+  end
 end
 
 def valid_phone_number?(phone)
