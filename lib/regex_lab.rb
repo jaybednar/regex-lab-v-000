edd29cd2ef1aa86e6108e32ce486204dd1ec8f6f
Jay Bednar <ironviralmedia@gmail.com>
2018-02-10 01:03:22 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
 def starts_with_a_vowel?(word)
-  !!word.match(/^[aeiouAEIOU]/) 
+  !!word.match(/^[aeiouAEIOU]/)
   # binding.pry
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\w\b+un+ing/)
+  text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
