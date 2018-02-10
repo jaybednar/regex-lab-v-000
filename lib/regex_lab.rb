83736dfaec6d05143160c50200f422a8aaabdbe1
Jay Bednar <ironviralmedia@gmail.com>
2018-02-10 01:18:19 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,7 +17,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
   if !!text.match(/\b[A-Z]\w+\W\D\b/)
     return true
   elsif !!text.match(/\b[A-Z]\w+ \b/)
-    return false 
+    return false
   # elsif !!text.match(/\b[a-z]\w+\W\D\b/)
   #   return false
   # elsif !!text.match(/\b[A-Z]\w\b/)
